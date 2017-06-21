fd9c5355b2b34d9a8d1eeef451e1bfbe9ef00695
Mohammed Shammout <fdsmohammed@gmail.com>
2017-06-21 05:51:46 +0300

    addding os check and brew install

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
+if [ "$(uname)" == "Darwin" ]; then
+  # macOS
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    echo "Installing Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+  else
+    echo "Updating Homebrew"
+    brew update
+  fi
+
+elif [ "$(uname)" == "Linux" ]; then
+  # Linux
+
+fi
+
+echo "Symlinking.."
 ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+
+echo
+echo "Symlinking Complete!"
+echo
