ad259b89f233e06221eef3b9c859cb31b762c888
Mohammed Shammout <fdsmohammed@gmail.com>
2017-06-21 06:30:13 +0300

    Add installing packages on macOS

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
-if [ "$(uname)" == "Darwin" ]; then
-  # macOS
-  which -s brew
-  if [[ $? != 0 ]] ; then
-    echo "Installing Homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
-  else
-    echo "Updating Homebrew"
-    brew update
-  fi
+case "$(uname)" in
+  Darwin)
+    which -s brew
+    if [[ $? != 0 ]]; then
+      echo "Installing Homebrew"
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+    else
+      echo "Updating Homebrew"
+      brew update
+    fi
 
-elif [ "$(uname)" == "Linux" ]; then
-  # Linux
-
-fi
+    echo "Installing Packages..."
+    for pkg in git tmux python3 neovim zsh; do
+      if brew list -1 | grep -q "^${pkg}\$"; then
+        echo "Package '$pkg' is installed"
+      else
+        echo "Installing package '$pkg'"
+        brew install $pkg;
+      fi
+    done
+    ;;
+  Linux)
+    ;;
+esac
 
 echo "Symlinking.."
 ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
